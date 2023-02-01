168abe37065c47d6bf4e3270e28fc591f93ac5bc
ash <ash5@gmail.com>
2023-02-01 15:20:04 +0530

    initial commit

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require("dotenv").config({ path: "./.env" });
 
 const app = express();
 app.use(express.json());
+app.use(express.static("public"));
 connect();
 app.use(cors());
 app.use("/user", require("./routes/userRoutes"));
